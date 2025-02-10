9a9a3c5ddac1a6034090733cd1c9f0b856fc5881
Isaias Gonçalves <131933508+IsaiasZaza@users.noreply.github.com>
2025-02-10 20:19:32 -0300

    feat: criando metodo de pagamento

---
+++ src/app/cancel/page.js
@@ -0,0 +1,13 @@
+// pages/cancel.js
+import React from 'react';
+
+const CancelPage = () => {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Pagamento Cancelado</h1>
+      <p>Você cancelou o processo de pagamento. Se isso foi um engano, por favor, tente novamente.</p>
+    </div>
+  );
+};
+
+export default CancelPage;
